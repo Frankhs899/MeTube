8802eddd33a05b80dcb75f78cac2b6a9c7aa8b6c
Franklin Hurtado Salazar <137368806+Frankhs899@users.noreply.github.com>
2025-03-08 20:10:09 -0500

    home page

--- core/settings.py
+++ core/settings.py
@@ -30,15 +30,24 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+]
+
+THIRD_PARTY_APPS =[
     'compressor',
 ]
+
+PROJECT_APPS =[
+    'download_file',
+]
+
+INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- core/urls.py
+++ core/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from .views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index'),
+    path('', home, name='home'),
 ]
